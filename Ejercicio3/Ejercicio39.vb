e90a9fdf40f1bc0e87d4432cf1b2a907ca27c278
Daniel baez <daniel.baez@outlook.es>
2017-05-13 22:59:41 -0300

    se ajusto el ejercicio39

--- Ejercicio3/Ejercicio39.vb
+++ Ejercicio3/Ejercicio39.vb
@@ -1,5 +1,5 @@
 ﻿Module Ejercicio39
-    Sub manin()
+    Sub main()
         Dim base, altura, lado As Double
         Console.WriteLine("ingrese la base: ")
         base = Console.ReadLine()
@@ -10,6 +10,7 @@
         Console.WriteLine("la superficie del cuadro es de: " & calculo(lado))
         Console.WriteLine("la superficie del rectangulo es de: " & calculo(base, altura))
         Console.WriteLine("la superficie del trapecio es de: " & calculo(base, altura, lado))
+        Console.ReadKey()
     End Sub
 
     Private Function calculo(numero As Double) As Double
@@ -21,7 +22,7 @@
     End Function
 
     Private Function calculo(numero As Double, numero2 As Double, numero3 As Double) As Double
-        Return ((numero + numero) / 2) * numero2
+        Return numero2 * ((numero + numero3) / 2)
     End Function
 
 End Module
